c8aecf7e57f1d37c1107d33610dd15b286188a40
Your Name <you@example.com>
2020-06-16 12:37:33 -0400

    locator

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class Card extends React.Component {
     const profile = this.props;
     return (
       <div className="github-profile">
-        <img src={profile.avatar_url} />
+        <img src={profile.avatar_url} alt="image" />
         <div className="info">
           <div className="name">{profile.name}</div>
           <div className="company">{profile.company}</div>
